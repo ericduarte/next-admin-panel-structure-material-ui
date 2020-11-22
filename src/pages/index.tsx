efd7c86adaed154ed92a1c469d94fde307d18561
Eric Duarte <ericduartemail@gmail.com>
2020-11-21 23:00:31 -0300

    Material UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from '../components/Layouts/DashBoard'
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <Layout>
       <h1>Hello Mundo</h1>
-    </div>
+    </Layout>
   )
 }
 export default Home
